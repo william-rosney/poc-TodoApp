0b6e0f63cbe3e6c2e3a19fee566e83703e8b5d23
William Rosney <willia.rosney11@gmail.com>
2023-08-17 12:22:24 -0400

    Modif for prod

--- api/Program.cs
+++ api/Program.cs
@@ -28,9 +28,9 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    //app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
